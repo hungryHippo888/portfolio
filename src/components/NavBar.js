795e86e579f27ea88ec4018fa2ec26fcc876621b
michaelyfu <m888fu@gmail.com>
2023-01-09 22:35:53 -0500

    went backt obrowser router

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import "../styles.scss";
 import "../App.css";
 import "./NavBar.css";
 import { Link, Route, Routes } from "react-router-dom";
-import { HashLink as HLink } from 'react-router-hash-link';
+// import { HashLink as HLink } from 'react-router-hash-link';
 import Logo from "../images/MF-logo.png";
 
 function NavBar() {
@@ -17,9 +17,9 @@ function NavBar() {
   return (
     <div className="navBar">
       <div className="navBar-left">
-        <HLink
+        <Link
           className="button"
-          to="/portfolio/#portfolio"
+          to="/portfolio"
           onClick={() => setCurrentLink("portfolio")}
         >
           <img
@@ -27,41 +27,41 @@ function NavBar() {
             style={{ height: "50px", width: "50px" }}
             alt="website logo"
           ></img>
-        </HLink>
+        </Link>
       </div>
       <div className="navBar-right">
-        <HLink
+        <Link
           style={currentLink === "photos" ? currStyle : defaultStyle}
           className="button"
           to="/photos"
           onClick={() => setCurrentLink("photos")}
         >
           <h3>Photos</h3>
-        </HLink>
-        <HLink
+        </Link>
+        <Link
           style={currentLink === "aboutMe" ? currStyle : defaultStyle}
           className="button"
-          to="/portfolio/#photos"
+          to="/aboutMe"
           onClick={() => setCurrentLink("aboutMe")}
         >
           <h3>About Me</h3>
-        </HLink>
-        <HLink
+        </Link>
+        <Link
           style={currentLink === "experience" ? currStyle : defaultStyle}
           className="button"
-          to="/portfolio/#experience"
+          to="/experience"
           onClick={() => setCurrentLink("experience")}
         >
           <h3>Experience</h3>
-        </HLink>
-        <HLink
+        </Link>
+        <Link
           style={currentLink === "projects" ? currStyle : defaultStyle}
           className="button"
-          to="/portfolio/#projects"
+          to="/projects"
           onClick={() => setCurrentLink("projects")}
         >
           <h3>Projects</h3>
-        </HLink>
+        </Link>
       </div>
     </div>
   );
